b3ba53594d034659e48c3ac3e93edf642b2d0e64
杨以豪 <2433152702@qq.com>
2019-07-23 16:37:51 +0800

    优化登录

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -15,7 +15,15 @@ function post() {
             if (response.code==200){
                 $("#comment_section").hide();
             } else{
-                alert(response.message);
+                if (response.code==2003){
+                    var isAccept = confirm(response.message);
+                    if (isAccept){
+                        window.open("https://github.com/login/oauth/authorize?client_id=1a853dfb75ee2be297d4&redirect_uri=http://localhost:8888/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable",true);
+                    }
+                } else {
+                    alert(response.message);
+                }
             }
         },
         dataType: "json"
