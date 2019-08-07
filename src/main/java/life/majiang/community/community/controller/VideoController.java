d3032255d9d5d725f5f4a10dcaf0897c7126bd74
杨以豪 <2433152702@qq.com>
2019-08-07 15:46:59 +0800

    初步实现视频详情界面

--- src/main/java/life/majiang/community/community/controller/VideoController.java
+++ src/main/java/life/majiang/community/community/controller/VideoController.java
@@ -2,6 +2,7 @@ package life.majiang.community.community.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class VideoController {
@@ -9,5 +10,10 @@ public class VideoController {
     @GetMapping("/videos")
     public String index(){
         return "videos";
+    }
+
+    @GetMapping("/video/{id}")
+    public String video(@PathVariable(name="id")String id){
+        return "video";
     }
 }
