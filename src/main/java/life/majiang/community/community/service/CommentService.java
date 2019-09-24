2476b8ce08e5ecac459353b44f3871616412b71d
杨以豪 <2433152702@qq.com>
2019-09-24 20:44:25 +0800

    修改回调地址

--- src/main/java/life/majiang/community/community/service/CommentService.java
+++ src/main/java/life/majiang/community/community/service/CommentService.java
@@ -12,7 +12,6 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
